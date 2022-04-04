a913f40358f4741f2966948ba32f4dd5bde0b5ce
yumeshino <shinozakiyume88@gmail.com>
2022-04-04 12:20:04 +0900

    13.phpの連想配列その3

--- index.php
+++ index.php
@@ -19,5 +19,55 @@ $array_member = [
 
 echo $array_member['hobby'];
 
+echo '<pre>';
+var_dump($array_member);
+echo '</pre>';
+
+//連想配列はキーの値にさらに配列を作って、その中にキーと値を作れる
+$array_member_2 = [
+    '本田' => [
+      'height' => 170,
+      'hobby' => 'サッカー'
+    ],
+    '香川' => [
+      'height' => 165,
+      'hobby' => 'サッカー'
+    ]
+  ];
+
+  echo $array_member_2['香川']['height'];
+
+  echo '<pre>';
+  var_dump($array_member_2);
+  echo '</pre>';
+
+  $array_member_3 = [
+    '1kumi' => [
+      '本田' => [
+        'height' => 170,
+        'hobby' => 'サッカー'
+      ],
+      '香川' => [
+        'height' => 165,
+        'hobby' => 'サッカー'
+      ]
+    ],
+    '2kumi' => [
+      '長友' => [
+        'height' => 160,
+        'hobby' => 'サッカー'
+      ],
+      '乾' => [
+        'height' => 168,
+        'hobby' => 'サッカー'
+      ]
+    ]
+  ];
+
+  echo $array_member_3['2kumi']['長友']['height'];
+
+   echo '<pre>';
+  var_dump($array_member_3);
+  echo '</pre>';
 
 ?>
