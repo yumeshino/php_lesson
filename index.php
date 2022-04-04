981b58b9b2eec630bacb7ec22e400f2f56b5d7ab
yumeshino <shinozakiyume88@gmail.com>
2022-04-04 16:35:30 +0900

    15.phpのif文その1

--- index.php
+++ index.php
@@ -1,73 +1,24 @@
 
 <?php
+//条件分岐
 
-//配列・・数字（順番固定）と値がセット
-//$array[1]
+//if(elseはできるだけ使わない)
+//foreach(配列の抽出)
+
+//if (条件) {
+//条件が真なら実行
+// } 
 
 
-//連想配列・・キーと値がセット
-//キー => 値
-//key => value
-//$array ["key"]
+$height = '90';//文字
 
-//キーと値の部分は文字ならシングルクォーテーション、数字は何もいない
-$array_member = [
-  'name' => '本田',
-  'height' => 170,
-  'hobby' => 'サッカー'
-];
+var_dump($height);
 
-echo $array_member['hobby'];
+if ($height === 90){//条件は数字
+  echo '身長は'.$height.'cmです';
+}
 
-echo '<pre>';
-var_dump($array_member);
-echo '</pre>';
-
-//連想配列はキーの値にさらに配列を作って、その中にキーと値を作れる
-$array_member_2 = [
-    '本田' => [
-      'height' => 170,
-      'hobby' => 'サッカー'
-    ],
-    '香川' => [
-      'height' => 165,
-      'hobby' => 'サッカー'
-    ]
-  ];
-
-  echo $array_member_2['香川']['height'];
-
-  echo '<pre>';
-  var_dump($array_member_2);
-  echo '</pre>';
-
-  $array_member_3 = [
-    '1kumi' => [
-      '本田' => [
-        'height' => 170,
-        'hobby' => 'サッカー'
-      ],
-      '香川' => [
-        'height' => 165,
-        'hobby' => 'サッカー'
-      ]
-    ],
-    '2kumi' => [
-      '長友' => [
-        'height' => 160,
-        'hobby' => 'サッカー'
-      ],
-      '乾' => [
-        'height' => 168,
-        'hobby' => 'サッカー'
-      ]
-    ]
-  ];
-
-  echo $array_member_3['2kumi']['長友']['height'];
-
-   echo '<pre>';
-  var_dump($array_member_3);
-  echo '</pre>';
+//== 一致
+//=== 型も一致
 
 ?>
