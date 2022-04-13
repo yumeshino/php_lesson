856cbfcd43d3560ac4ac9ac2cc96b604cf7886c3
yumeshino <shinozakiyume88@gmail.com>
2022-04-13 11:55:21 +0900

    18.phpのforEach

--- index.php
+++ index.php
@@ -1,45 +1,29 @@
 
 <?php
 
-// foreach 複数の値を展開したり表示したりする
+// for 繰り返す数が決まっていたら
+//  while 繰り返す数が決まっていなっかたら
 
-$members = [
-  'name' => '本田',
-  'height' => '170',
-  'hibby' => 'サッカー'
-];
-
-//バリューのみ表示
-//右側の変数は何でもいいが視認性の観点から元々の名前を複数形にして右側は単体形にするのがオーソドックス
-foreach($members as $member){
-  echo $member;
+// continue,break
+for($i = 0; $i < 10; $i++ ){
+  if($i === 5){
+    // break
+    // continue
+  }
+  echo $i;
 }
 
-echo'<br>';
 
-//キーとバリューそれぞれ表示
-foreach($members as $key => $value){
-  echo $key. 'は'. $value. 'です';
+echo '<br>';
+
+
+$j = 0;
+while($j < 5){
+  echo $j;
+  $j++;
 }
 
-echo'<br>';
 
 
 
-// 多段階の配列を展開
-$members_2 = [
-  '本田' => [
-    'height' => '170',
-    'hibby' => 'サッカー'
-  ],
-  '香川' => [
-    'height' => '165',
-    'hibby' => 'サッカー'
-  ]
-];
-
-foreach($members_2 as $member_1){
-  foreach($member_1 as $member => $value)
-  echo $member. 'は'. $value. 'です';
-}
 ?>
