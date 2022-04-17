1d6446c92f6cc5b640f171cdc71f07fc41c25314
yumeshino <shinozakiyume88@gmail.com>
2022-04-17 22:38:22 +0900

    28.変数スコープ

--- scope.php
+++ scope.php
@@ -13,12 +13,7 @@ function checkScope(){
 //$localVariableを表示させたい場合はその関数を呼び出す
 checkScope();
 
-//グローバル変数をスコープ内で表示させたい場合
-//引数をインプットで表示させる
-function checkScope2($str){
-  $localVariable = 'ローカル変数です';
-  echo $str;
-}
+
 
 checkScope2($globalVariable);
 
