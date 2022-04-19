a27b0746cbad36ebe2358d0474d120bd07804706
yumeshino <shinozakiyume88@gmail.com>
2022-04-19 11:46:42 +0900

    29.ファイルの読み込み

--- common.php
+++ common.php
@@ -7,6 +7,12 @@ function commonTest(){
   echo'外部ファイルの関数です';
 }
 
+// require();require_once();// エラー出ると処理が止まる
+// include();include_oncce();//警告が出て処理が続けられる
 
+// __DIR__ __FILE__マジック定数
+
+//他の方法で名前空間(namespace)もあり
+// ・・Laravelなどのフレームワークはこちらが主流
 
 ?>
