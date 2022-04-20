46c4eea1f1b6db577494141720bb39c74ee2896f
yumeshino <shinozakiyume88@gmail.com>
2022-04-20 10:46:45 +0900

    34.フォーム入力、確認、完了

--- form/input.php
+++ form/input.php
@@ -1,4 +1,8 @@
 <?php
+//スーパーグローバル変数 php 9種類
+//連想配列になっている
+//GET..アドレス通信にキーが表示される
+//POST..アドレス通信に何も表示されない 表示されてはまずいもの（パスワードなど）のときに使う
 
 if(!empty($_POST)){
   echo '<pre>';
@@ -6,10 +10,19 @@ if(!empty($_POST)){
   echo '</pre>';
 }
 
-//スーパーグローバル変数 php 9種類
-//連想配列になっている
-//GET..アドレス通信にキーが表示される
-//POST..アドレス通信に何も表示されない 表示されてはまずいもの（パスワードなど）のときに使う
+//入力、確認、完了 input.php,conform.php,thanks.php
+//input.php
+
+$pageFlag = 0;
+
+if(!empty($_POST['btn_confirm'])){
+  $pageFlag = 1;
+}
+
+if(!empty($_POST['btn_submit'])){
+  $pageFlag = 2;
+}
+
 ?>
 
 
@@ -21,17 +34,40 @@ if(!empty($_POST)){
   <title>input</title>
 </head>
 <body>
+
+<?php if($pageFlag === 1) : ?>
   <form method="POST" action="input.php">
     氏名
-    <input type="text" name="yuor_name">
+    <?php echo $_POST['your_name'];?>
     <br>
-    <input type="checkbox" name="sports[]" value="野球">野球
-    <input type="checkbox" name="sports[]" value="サッカー">サッカー
-    <input type="checkbox" name="sports[]" value="バスケ">バスケ
-
-    <input type="submit" value="送信">
-
+    メールアドレス
+    <?php echo $_POST['email'];?>
+    <br>
+    <input type="submit" name="btn_submit" value="送信する">
+    <input type="hidden" name="your_name" value=" <?php echo $_POST['your_name'];?>">
+    <input type="hidden" name="email" value=" <?php echo $_POST['email'];?>">
   </form>
+<?php endif; ?>
+
+
+<?php if($pageFlag === 2) : ?>
+  送信が完了しました。
+<?php endif; ?>
+
+
+<?php if($pageFlag === 0) : ?>
+  <form method="POST" action="input.php">
+    氏名
+    <input type="text" name="your_name">
+    <br>
+    メールアドレス
+    <input type="email" name="email">
+    <br>
+    <input type="submit" name="btn_confirm" value="確認する">
+  </form>
+  <?php endif; ?>
+
+
   
 </body>
 </html>
