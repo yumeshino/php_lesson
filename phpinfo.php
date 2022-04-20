fb20e95113a180fa0ef931db86f0bb280ad0e3be
yumeshino <shinozakiyume88@gmail.com>
2022-04-20 09:26:51 +0900

    32.処理を止める

--- phpinfo.php
+++ phpinfo.php
@@ -4,4 +4,15 @@
 phpinfo();
 //Configuration File (php.ini) Pathに記載されているファイル名から設定の変更が可能
 
+
+//処理を止める処理
+//コードが長くなった場合に変数の中に何の値が入っているかを確認する時などにexitを使って処理を止めて確認する
+$test = 123;
+$test_2 = 456;
+
+echo $test;
+var_dump($test);
+exit;
+echo $test_2;
+
 ?>
