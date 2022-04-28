88a0a1196224f04c98972a970f8a4d278b7b6b39
yumeshino <shinozakiyume88@gmail.com>
2022-04-28 14:13:26 +0900

    41.バリテージョン

--- form/validation.php
+++ form/validation.php
@@ -4,7 +4,25 @@ function validation($request){//$_POST連想配列
 
   $errors = [];
 
+  if(empty($request['your_name']) || 20 < mb_strlen($request['your_name'])){
+    $errors[] = '「氏名」は必須です。20文字以内で入力してください。';
+  }
 
+  if(!isset($request['gender'])){
+    $errors[] = '「性別」は必須です。';
+  }
+
+  if(empty($request['age']) || 6 < $request['age']){
+    $errors[] = '「年齢」は必須です。';
+  }
+
+  if(empty($request['contact']) || 200 < mb_strlen($request['contact'])){
+    $errors[] = '「お問い合わせ内容」は必須です。200文字以内で入力してください。';
+  }
+
+  if(empty($request['caution'])){
+    $errors[] = '「注意事項」をご確認ください。';
+  }
 
 
   return $errors;
