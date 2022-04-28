9af06e27e4011412097df2a448984959f6837959
yumeshino <shinozakiyume88@gmail.com>
2022-04-28 14:12:43 +0900

    40.バリテージョン

--- form/input.php
+++ form/input.php
@@ -56,8 +56,9 @@ function h($str)
 //input.php
 
 $pageFlag = 0;
+$errors = validation($_POST);
 
-if(!empty($_POST['btn_confirm'])){
+if(!empty($_POST['btn_confirm']) && empty($errors)){
   $pageFlag = 1;
 }
 
@@ -117,7 +118,6 @@ if(!empty($_POST['btn_submit'])){
     <input type="hidden" name="csrf" value=" <?php echo h($_POST['csrf']);?>">
   </form>
 <?php endif; ?>
-
 <?php endif; ?>
 
 
@@ -138,7 +138,20 @@ if(!empty($_POST['btn_submit'])){
     $_SESSION['csrfToken'] = $csrfToken;
   }
   $token = $_SESSION['csrfToken'] = $csrfToken;
-  ?>
+?>
+
+<?php if(!empty($errors) && !empty($_POST['btn_confirm']) ) : ?>
+  <?php echo '<ul>' ;?>
+    <?php
+      foreach($errors as $error){
+        echo '<li>' . $error . '</li>';
+      }
+    ?>
+  <?php echo '</ul> ;?>
+
+<?php endif; ?>
+
+
   <form method="POST" action="input.php">
     氏名
     <input type="text" name="your_name" value="<?php if(!empty( $_POST['your_name'])){echo h($_POST['your_name']);}?>">
@@ -151,16 +164,16 @@ if(!empty($_POST['btn_submit'])){
     <br>
     性別
     <input type="radio" name="gender" value="0" 
-    <?php if(!empty( $_POST['gender']) && $_POST['gender'] === '0')
+    <?php if(isset( $_POST['gender']) && $_POST['gender'] === '0')
     {echo 'checked';}?>>男性
     <input type="radio" name="gender" value="1"
-    <?php if(!empty( $_POST['gender']) && $_POST['gender'] === '1')
+    <?php if(isset( $_POST['gender']) && $_POST['gender'] === '1')
     {echo 'checked';}?>>女性
     <br>
     年齢
     <select name="age">
       <option value="0">選択してください</option>
-      <option value="1">〜19歳</option>
+      <option value="1" selected>〜19歳</option>
       <option value="2">20〜29歳</option>
       <option value="3">30〜39歳</option>
       <option value="4">40〜49歳</option>
@@ -179,9 +192,8 @@ if(!empty($_POST['btn_submit'])){
     <input type="submit" name="btn_confirm" value="確認する" >
     <input type="hidden" name="csrf" value="<?php echo $token; ?>">
   </form>
+
   <?php endif; ?>
 
-
-  
 </body>
 </html>
--- form/validation.php
+++ form/validation.php
@@ -4,9 +4,8 @@ function validation($request){//$_POST連想配列
 
   $errors = [];
 
-  if(empty($request['yuor_name'])){
-    $errors[] = '氏名は必須です';
-  }
+
+
 
   return $errors;
 
