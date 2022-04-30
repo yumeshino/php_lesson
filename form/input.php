fd6b59aa077d44f667c43049ab4740c0ceea8833
yumeshino <shinozakiyume88@gmail.com>
2022-04-30 22:00:00 +0900

    42.bootstrapその1

--- form/input.php
+++ form/input.php
@@ -156,12 +156,18 @@ if(!empty($_POST['btn_submit'])){
 
 <?php endif;?>
 
-
-<form method="POST" action="input.php">
-  <div class="form-group">
-  <label for="your_name">氏名</label>
-   <input type="text" class="form_control" id="your_name" value="<?php if(!empty( $_POST['your_name'])){echo h($_POST['your_name']);}?>" required> 
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+    <form method="POST" action="input.php">
+      <div class="form-group">
+        <label for="your_name">氏名</label>
+        <input type="text" class="form_control" id="your_name" value="<?php if(!empty( $_POST['your_name'])){echo h($_POST['your_name']);}?>" required> 
+      </div>
+      </div>
+    </div>
   </div>
+
     メールアドレス
     <input type="text" name="email" value="<?php if(!empty( $_POST['email'])){echo h($_POST['email']);}?>">
     <br>
@@ -208,10 +214,9 @@ if(!empty($_POST['btn_submit'])){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
+    
 
   </body>
 </html>
