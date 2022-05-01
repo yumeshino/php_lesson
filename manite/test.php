0d6c66060dc73e4cd2c51b4b04442cad0d2ff2bb
yumeshino <shinozakiyume88@gmail.com>
2022-05-01 17:14:33 +0900

    45.ファイル操作

--- manite/test.php
+++ manite/test.php
@@ -17,4 +17,50 @@ echo '<br>';
 //パスワード（暗号化）
 //第一引数に実際のパスワード、第二引数にパウワードの暗号化のタイプ
 echo(password_hash('password123',PASSWORD_BCRYPT));
+
+
+// phpでデータを保存する方法
+// ・ファイル（テキストファイル）
+//     ->手軽・データのやりとり
+
+// ・データベース（MySQL、MariaDB）
+//     ->大量のデータを保管
+
+
+// ファイルの操作
+// ・ファイル名型（ファイル丸ごと）
+//     ->file_get_contents,file_put_contents
+// ・ストリーム型（一行ごと）
+//     ->fopen,fclose,fgets.fwrite
+// ・オブジェクト型（オブジェクトとして）
+//     ->SpFileObject    
+
+
+$contactFile = '.contact.dat';
+
+//ファイル丸ごと読み込み
+$fileContents = file_get_contents($contactFile);
+
+// echo $fileContents;
+
+//ファイルに読み込み（上書き）
+// file_put_contents($contactFile,'テストです');
+
+//追加する文章を改行したい場合
+$addText = 'テストです' . "\n";
+
+//ファイルに書き込み（追記）
+file_put_contents($contactFile, $addText ,FILE_APPEND);
+
+
+//配列 file , 区切る explode,  foreach
+$allDate = file($contactFile);
+
+foreach($allDate as $lineDate){
+  $lines = explode(',',$lineDate);
+  echo $lines[0].'<br>';
+  echo $lines[1].'<br>';
+  echo $lines[2].'<br>';
+}
+
 ?>
