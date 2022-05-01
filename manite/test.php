dd657c14992c21573676850d458f0b741b3d44f5
yumeshino <shinozakiyume88@gmail.com>
2022-05-01 17:35:02 +0900

    46.ファイル操作その2

--- manite/test.php
+++ manite/test.php
@@ -10,15 +10,15 @@ htaccessファイルで指定
 
 <?php
 //パスワードを記録したファイルの場所
-echo __FILE__;
+// echo __FILE__;
 // /Applications/MAMP/htdocs/php_test/manite/test.php
 
-echo '<br>';
+// echo '<br>';
 //パスワード（暗号化）
 //第一引数に実際のパスワード、第二引数にパウワードの暗号化のタイプ
-echo(password_hash('password123',PASSWORD_BCRYPT));
+// echo(password_hash('password123',PASSWORD_BCRYPT));
 
-
+// -----------------------------------------------------------------
 // phpでデータを保存する方法
 // ・ファイル（テキストファイル）
 //     ->手軽・データのやりとり
@@ -36,10 +36,10 @@ echo(password_hash('password123',PASSWORD_BCRYPT));
 //     ->SpFileObject    
 
 
-$contactFile = '.contact.dat';
+// $contactFile = '.contact.dat';
 
 //ファイル丸ごと読み込み
-$fileContents = file_get_contents($contactFile);
+// $fileContents = file_get_contents($contactFile);
 
 // echo $fileContents;
 
@@ -47,20 +47,39 @@ $fileContents = file_get_contents($contactFile);
 // file_put_contents($contactFile,'テストです');
 
 //追加する文章を改行したい場合
-$addText = 'テストです' . "\n";
+// $addText = 'テストです' . "\n";
 
 //ファイルに書き込み（追記）
-file_put_contents($contactFile, $addText ,FILE_APPEND);
+// file_put_contents($contactFile, $addText ,FILE_APPEND);
 
 
 //配列 file , 区切る explode,  foreach
-$allDate = file($contactFile);
+// $allDate = file($contactFile);
 
-foreach($allDate as $lineDate){
-  $lines = explode(',',$lineDate);
-  echo $lines[0].'<br>';
-  echo $lines[1].'<br>';
-  echo $lines[2].'<br>';
-}
+// foreach($allDate as $lineDate){
+//   $lines = explode(',',$lineDate);
+//   echo $lines[0].'<br>';
+//   echo $lines[1].'<br>';
+//   echo $lines[2].'<br>';
+// }
+
+// -----------------------------------------------------------
+//phpファイルの操作機能
+
+$contactFile = '.contact.dat';
+
+$contents = fopen($contactFile,'a+');
+
+$addText = '1行追記' . "\n";
+
+fwrite($contents,$addText);
+
+fclose($contents);
+
+//ファイル操作の流れ（ストリーム型）
+// 1,開く fopen(r,w,a)
+// 2,排他ロック flock
+// 3,読込/書込/追記 fgets/fwrite
+// 4,閉じる fclose（ロック解除）
 
 ?>
