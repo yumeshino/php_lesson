f66b6f93abc6337466f46f5d7565a36d4baf0536
yumeshino <shinozakiyume88@gmail.com>
2022-05-11 12:45:12 +0900

    54.フォーム値をDBに保存その2

--- manite/db_connection.php
+++ manite/db_connection.php
@@ -1,13 +1,15 @@
 <?php
 
-const DB_HOST = 'mysql:dbname=udemy_php;host=127.0.0.1;charset=utf8';
-const DB_USER = 'php_user';
-const DB_PASSWORD = 'password123';
+const DB_HOST = 'mysql:dbname=udemy_php;host=localhost;charset=utf8';
+const DB_USER = 'root';
+const DB_PASSWORD = 'root';
 
 
 //例外処理 Exception
 //データベースを繋ぐときにはお決まりの書き方
 try{
+  //基本のデフォルト値
+  //$pdo = new PDO('mysql:host=localhost;dbname=example', 'root', 'root');
   $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [ 
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,//連想配列
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,//例外を表示※必須
