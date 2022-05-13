7244818ccf68073c6be2091520e9cb0d56fc61df
yumeshino <shinozakiyume88@gmail.com>
2022-05-13 12:44:04 +0900

    復習の追記

--- manite/db_connection.php
+++ manite/db_connection.php
@@ -12,11 +12,11 @@ try{
   //$pdo = new PDO('mysql:host=localhost;dbname=example', 'root', 'root');
 
   // PDOとDBを繋ぐための実体化（インスタンス化）
+  //４番目の引数はオプション・[]で配列形式で書く
   $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [ 
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,//連想配列
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,//例外を表示※必須
     PDO::ATTR_EMULATE_PREPARES => false,//SQLインジェクション対策※必須
-
   ]);
   echo '接続成功';
 
