7802da617f5607230374bdbb019825a477d9042b
yumeshino <shinozakiyume88@gmail.com>
2022-05-13 12:37:55 +0900

    復習の追記

--- manite/db_connection.php
+++ manite/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-// PDOとDBを繋ぐための実体化（インスタンス化）
+
 const DB_HOST = 'mysql:dbname=udemy_php;host=localhost;charset=utf8';
 const DB_USER = 'php_user';
 const DB_PASSWORD = 'moyumi090611';
@@ -11,6 +11,8 @@ const DB_PASSWORD = 'moyumi090611';
 try{
   //初期値は host が localhost、user が root、pass が root 
   //$pdo = new PDO('mysql:host=localhost;dbname=example', 'root', 'root');
+
+  // PDOとDBを繋ぐための実体化（インスタンス化）
   $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [ 
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,//連想配列
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,//例外を表示※必須
