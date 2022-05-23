922c947a6cfa41d36740aad01efd153a4c234c15
yumeshino <shinozakiyume88@gmail.com>
2022-05-23 12:23:45 +0900

    64.トレイトの補足

--- object/trait.php
+++ object/trait.php
@@ -1,21 +1,23 @@
 <?php
 
+//頭にtrait
+//classが単一継承なのに対して複数の継承が可能
 trait ProductTrait{
-
   public function getProduct(){
     echo 'プロダクト';
   }
 }
 
 trait NewsTrait{
-
   public function getNews(){
     echo 'ニュース';
   }
 }
 
+
 class Product{
 
+  //useというキーワードでtraitの機能を持ってくる
   use ProductTrait;
   use NewsTrait;
 
@@ -23,11 +25,14 @@ class Product{
     echo 'クラスです';
   }
 
+//オーバーライドも可能
+//その場合classの方のメソッドが優先される
   // public function getNews(){
   //   echo 'クラスのニュースです';
   // }
 }
 
+//instance化
 $product = new Product();
 
 $product->getInformation();
