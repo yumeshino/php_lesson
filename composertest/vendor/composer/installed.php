7acc18a7ca762a19ce08ab4c984fefc0cbafe78e
yumeshino <shinozakiyume88@gmail.com>
2022-05-28 19:46:29 +0900

    66.composerの実行

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '34345b351608f951be305f4e9f4498918841c192',
+        'name' => 'shinozakiyume/composertest',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shinozakiyume/composertest' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '34345b351608f951be305f4e9f4498918841c192',
+            'dev_requirement' => false,
+        ),
+    ),
+);
